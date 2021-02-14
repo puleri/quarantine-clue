a89621bbe34bf16bd74918f789be8d34bf9fb5f9
mp4 <puleri.mp4@gmail.com>
2021-02-14 13:25:08 -0800

    Added deck, card, and player class
    Along with creating the clue deck, drawing
    cards it now functional.

--- client.py
+++ client.py
@@ -11,15 +11,53 @@ pygame.display.set_caption("Client")
 
 clientNumber = 0
 
+class Card:
+    def __init__(self, name):
+        self.name = name
+
+class Deck():
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+
+    def build(self):
+        for i in ["Kitchen", "Ballroom", "Conservatory", "Dining Room", "Lounge", "Hall", "Study", "Library", "Billiard Room", "Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Wrench", "Miss Scarlett", "Rev Green", "Colonel Mustard", "Professor Plum", "Mrs. Peacock", "Mrs. White"]:
+            self.cards.append(Card(i))
+
+    def length(self):
+        return len(self.cards)
+
+    def return_cards(self, cards):
+        self.cards.extend(cards)
+
+class Game:
+    def __init__(self, players, deck):
+        self.players = players
+        self.deck = deck
+        self.cards = deck.cards
+
+    def deal(self):
+        for player in self.players:
+            for i in range(2):
+                player.cards.append(self.drawCard())
+
+    def drawCard(self):
+        drawnCard = self.deck.cards[0]
+        self.deck.cards = self.deck.cards[1:]
+        return drawnCard
+
 class Player():
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x,y,width,height)
-        self.vel = 3
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+        # self.x = x
+        # self.y = y
+        # self.width = width
+        # self.height = height
+        # self.color = color
+        # self.rect = (x,y,width,height)
+        # self.vel = 3
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
@@ -40,22 +78,34 @@ class Player():
 
         self.rect = (self.x, self.y, self.width, self.height)
 
-def redrawWindow(win, player):
-    win.fill((255,255,255))
-    player.draw(win)
-    pygame.display.update()
+def return_cards(self, deck):
+    deck.return_cards(self.cards)
+    self.cards = []
 
-def main():
-    run = True
-    p = Player(50,50,100,100,(0,255,0))
-    clock = pygame.time.Clock()
+# def redrawWindow(win, player):
+#     win.fill((255,255,255))
+#     player.draw(win)
+#     pygame.display.update()
+# 
+# def main():
+#     run = True
+#     p = Player(50,50,100,100,(0,255,0))
+#     clock = pygame.time.Clock()
+#
+#     while run:
+#         clocl.tick(60)
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 run = False
+#                 pygame.quit()
+#         p.move()
+#         redrawWindow(win, p)
+# main()
 
-    while run:
-        clocl.tick(60)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-        p.move()
-        redrawWindow(win, p)
-main()
+clueDeck = Deck()
+matt = Player("Matt")
+jo = Player("Jo")
+newGame = Game([matt, jo], clueDeck)
+newGame.deal()
+print(jo.cards[0].name, jo.cards[1].name, matt.cards[0].name, matt.cards[1].name)
+return_cards(clueDeck, clueDeck)
