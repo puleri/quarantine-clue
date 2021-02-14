4c58db0309c8310d6550928313291f6f6201c30e
mp4 <puleri.mp4@gmail.com>
2021-02-14 11:21:09 -0800

    Added pygame file and built moving square
    Added pygame library and will be testing for
    ways to create clue cards.

--- client.py
+++ client.py
@@ -1,84 +1,61 @@
-import socket
-import select
-import errno
+import pygame
 
-HEADER_LENGTH = 10
+# run with python client
 
-IP = "127.0.0.1"
-PORT = 1234
-my_username = input("Username: ")
+width = 500
+height = 500
 
-# Create a socket
-# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+win = pygame.display.set_mode((width, height))
 
-# Connect to a given ip and port
-client_socket.connect((IP, PORT))
+pygame.display.set_caption("Client")
 
-# Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
-client_socket.setblocking(False)
+clientNumber = 0
 
-# Prepare username and header and send them
-# We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
-username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-client_socket.send(username_header + username)
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x,y,width,height)
+        self.vel = 3
 
-while True:
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
 
-    # Wait for user to input a message
-    message = input(f'{my_username} > ')
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
 
-    # If message is not empty - send it
-    if message:
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
 
-        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
-        message = message.encode('utf-8')
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(message_header + message)
+        if keys[pygame.K_UP]:
+            self.y -= self.vel
 
-    try:
-        # Now we want to loop over received messages (there might be more than one) and print them
-        while True:
+        if keys[pygame.K_DOWN]:
+            self.y += self.vel
 
-            # Receive our "header" containing username length, it's size is defined and constant
-            username_header = client_socket.recv(HEADER_LENGTH)
+        self.rect = (self.x, self.y, self.width, self.height)
 
-            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
-            if not len(username_header):
-                print('Connection closed by the server')
-                sys.exit()
+def redrawWindow(win, player):
+    win.fill((255,255,255))
+    player.draw(win)
+    pygame.display.update()
 
-            # Convert header to int value
-            username_length = int(username_header.decode('utf-8').strip())
+def main():
+    run = True
+    p = Player(50,50,100,100,(0,255,0))
+    clock = pygame.time.Clock()
 
-            # Receive and decode username
-            username = client_socket.recv(username_length).decode('utf-8')
-
-            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
-            message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = client_socket.recv(message_length).decode('utf-8')
-
-            # Print message
-            print(f'{username} > {message}')
-
-    except IOError as e:
-        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
-        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
-        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
-        # If we got different error code - something happened
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error: {}'.format(str(e)))
-            sys.exit()
-
-        # We just did not receive anything
-        continue
-
-    except Exception as e:
-        # Any other exception - something happened, exit
-        print('Reading error: '.format(str(e)))
-        sys.exit()
-
-# Reference: Feb 2021 https://www.youtube.com/watch?v=ytu2yV3Gn1I
+    while run:
+        clocl.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+        p.move()
+        redrawWindow(win, p)
+main()
