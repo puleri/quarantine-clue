df5ebd2b29cc4954a2999be502522ad95dee21bb
mp4 <puleri.mp4@gmail.com>
2021-02-21 20:00:15 -0800

    Added multiple player lists
    Running the file now deals to 4 players and prints the hand of
    player 1. Each player gets dealt a new hand every time the file
    is run.

--- client.py
+++ client.py
@@ -1,5 +1,12 @@
 from random import shuffle
 
+player1 = []
+player2 = []
+player3 = []
+player4 = []
+
+players = [player1, player2, player3, player4]
+
 class Card:
     def __init__(self,name):
         self.name = name
@@ -16,6 +23,29 @@ for n in name: #This is the code that actually makes a deck
 
 shuffle(name)
 for card in range(21):
+    counter = 1
         #deal cards here using deck.pop()
         # print(deck.pop().__str__)  #just to prove it works randomly =P
-        print(name[card])
+    for player in players:
+        if (name.index(name[card]) == 1):
+            player.append(name[card])
+            counter += 1
+            break
+        if (name.index(name[card]) == 2):
+            player.append(name[card])
+            counter += 1
+            break
+        if (name.index(name[card]) == 3):
+            player.append(name[card])
+            counter += 1
+            break
+        if (name.index(name[card]) == 4):
+            player.append(name[card])
+            counter = 0
+            break
+
+            # player.append(name[card])
+            print(name[card])
+            break
+
+print(player1)
