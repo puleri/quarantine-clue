4efb390caa2f4eb91142aece0f6b9f34b9d85df3
mp4 <puleri.mp4@gmail.com>
2021-02-22 16:10:12 -0800

    Fixed issue with cards not being dealt to all four players
    Random deal now gives the first two players 5 cards, and the
    last two players 4 cards. The deals do not yet support the
    'case file' deal yet.

--- client.py
+++ client.py
@@ -22,30 +22,44 @@ for n in name: #This is the code that actually makes a deck
     deck.append(Card(n))
 
 shuffle(name)
-for card in range(21):
-    counter = 1
-        #deal cards here using deck.pop()
-        # print(deck.pop().__str__)  #just to prove it works randomly =P
-    for player in players:
-        if (name.index(name[card]) == 1):
-            player.append(name[card])
-            counter += 1
-            break
-        if (name.index(name[card]) == 2):
-            player.append(name[card])
-            counter += 1
-            break
-        if (name.index(name[card]) == 3):
-            player.append(name[card])
-            counter += 1
-            break
-        if (name.index(name[card]) == 4):
-            player.append(name[card])
-            counter = 0
-            break
 
-            # player.append(name[card])
-            print(name[card])
-            break
+for i in range(5):
+    player1.append(name[i])
+    name.remove(name[i])
+for j in range(5):
+    player2.append(name[j])
+    name.remove(name[j])
+for k in range(4):
+    player3.append(name[k])
+    name.remove(name[k])
+for l in range(4):
+    player4.append(name[l])
+    name.remove(name[l])
+# for card in range(21):
+#     counter = 1
+#         #deal cards here using deck.pop()
+#         # print(deck.pop().__str__)  #just to prove it works randomly =P
+#     for player in players:
+#         if (name.index(name[card]) == 1):
+#             player.append(name[card])
+#             counter += 1
+#             break
+#         if (name.index(name[card]) == 2):
+#             player.append(name[card])
+#             counter += 1
+#             break
+#         if (name.index(name[card]) == 3):
+#             player.append(name[card])
+#             counter += 1
+#             break
+#         if (name.index(name[card]) == 4):
+#             player.append(name[card])
+#             counter = 0
+#             break
+#
+#             # player.append(name[card])
+#             print(name[card])
+#             break
 
-print(player1)
+print("Player 1's hand: ", player1, "\nPlayer 2's hand: ",player2,"\nPlayer 3's hand: ", player3,"\nPlayer 4's hand: ", player4)
+print(name)
