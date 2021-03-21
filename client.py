a78b7d13387b66131d1d80623fe60a7f27bcb8ed
mp4 <puleri.mp4@gmail.com>
2021-03-21 11:46:41 -0700

    Minor formatting/styling
    Added the rich python library to add some minor styling
    to the terminal.

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
 from random import shuffle, randint
+from rich.console import Console
+
+console = Console()
 
 # instantiate the individual player lists, combined player list, and the crime_file
 player1 = []
@@ -61,22 +64,23 @@ while count < 4:
 # print("THE CRIME FILE: ", crime_file)
 
 def main():
-    input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nQuarentine Clue\n\nThe Rules: In the dealing phase each player must write down their cards on seperate pieces of paper. When asking revealing clues then the whole group must allow the two parties a private showing. All applicable Clue rules still apply. This is currently in Beta, so any feedback is appreciated! Keep on your toes and enjoy detectives!\n\n\n\n\n\n\n\n\n\n\n\n")
+    console.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nQuarentine Clue\n\n", style="blue")
+    input("The Rules: In the dealing phase each player must write \ndown their cards on seperate pieces of paper. When asking revealing clues then \nthe whole group must allow the two parties a private showing. All applicable Clue rules still apply. \nThis is currently in Beta, so any feedback is appreciated! Keep on your toes and \nenjoy detectives!(press enter to continue...)\n\n\n\n\n\n\n\n\n\n\n\n")
     input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress enter for Player 1's hand...\n\n\n\n\n\n\n\n\n")
-    input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHere comes your hand Player 1! Keep it a secret and press enter when you are ready to pass...\n\n\n\n\n\n\n\n\n\n")
-    input(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 1 write this down! Keep your eye on the player on the top left of your zoom screen. They are acting suspicious.\n\nPLAYER ONE'S HAND: {player1}\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress enter when your records are straight.")
+    input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHere comes your hand Player 1! Keep it a secret and press enter \nwhen you are ready to pass...\n\n\n\n\n\n\n\n\n\n")
+    input(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 1 write this down! Keep your eye on the player on the top left \nof your zoom screen. They are acting suspicious.\n\nPLAYER ONE'S HAND: {player1}\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress enter when your records are straight.")
 
-    input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer two here comes your deal. Make sure no one else can see. Trust no one. Press enter to continue...\n\n\n\n\n\n\n\n\n")
+    input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer two here comes your deal. Make sure no one else can see. \nTrust no one. Press enter to continue...\n\n\n\n\n\n\n\n\n")
     input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nKeep this close to your chest and press enter when you are ready to pass...\n\n\n\n\n\n\n\n\n\n")
     input(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 2 write this down! Watch out for the top right player on the call. They are shifty.\n\nPLAYER TWO'S HAND: {player2}\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress enter when your records are straight.")
 
     input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress enter for Player 3's hand...\n\n\n\n\n\n\n\n\n")
     input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHere comes your hand Player 3! Keep it a secret and press enter when you are ready to pass...\n\n\n\n\n\n\n\n\n\n")
-    input(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 3 write this down! Keep your eye on the player on the bottom left of your zoom screen. They are acting suspicious.\n\nPLAYER THREE'S HAND: {player3}\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress enter when your records are straight.")
+    input(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 3 write this down! Keep your eye on the player on the \nbottom left of your zoom screen. They are acting suspicious.\n\nPLAYER THREE'S HAND: {player3}\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress enter when your records are straight.")
 
-    input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer four here comes your deal. Make sure no one else can see. Trust no one. Press enter to continue...\n\n\n\n\n\n\n\n\n")
-    input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nKeep this close to your chest and press enter when you are ready to start...\n\n\n\n\n\n\n\n\n\n")
-    input(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 4 write this down! Watch out for the top right player on the call. They are shifty.\n\nPLAYER FOUR'S HAND: {player4}\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress enter when your records are straight.")
+    input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer four here comes your deal. Make sure no one else can see. \nTrust no one. Press enter to continue...\n\n\n\n\n\n\n\n\n")
+    input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nKeep this close to your chest and press enter when \nyou are ready to start...\n\n\n\n\n\n\n\n\n\n")
+    input(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 4 write this down! Watch out for the top right player \non the call. They are shifty.\n\nPLAYER FOUR'S HAND: {player4}\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress enter when your records are straight.")
 
     input(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nTHE CASE FILE\n\n\n\n\n\n\n\n\n\n\n\n(press enter to open/close)")
     input(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nTHE CASE FILE: {crime_file}\n\n\n\n\n\n\n\n\n\n\n\n(press enter to open/close)")
